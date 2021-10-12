d934444ae94ea606ac1f9583668f883d8da0a1b3
Casper Yonel <yonelcasper@gmail.com>
2021-10-12 14:55:38 -0600

    Trying again

--- calc.js
+++ calc.js
@@ -22,6 +22,8 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(num1 + num2);
 	}
 
+	//New commit
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
