972fdad5dabd7242959451ab358a6f7ea4cd9b08
Casper Yonel <yonelcasper@gmail.com>
2021-10-12 15:53:47 -0600

    2nd commit?

--- calc.js
+++ calc.js
@@ -14,15 +14,22 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
-	console.log('math symbol', mathSymbol)
-	console.log('num1', num1)
-	console.log('num2', num2)
+	// console.log('math symbol', mathSymbol)
+	// console.log('num1', num1)
+	// console.log('num2', num2)
 
 	if (mathSymbol === "+"){
-		console.log(num1 + num2);
-	else if (mathSymbol === "-");
-		console.log(num1 - num2);
-	}
+			console.log(num1 + num2)
+	}	else if (mathSymbol === "-"){
+			console.log(num1 - num2)
+	}	else if (mathSymbol === "*"){
+			console.log(num1 * num2)
+	}	else if (mathSymbol === "/"){
+			console.log(num1 / num2)
+	}	else if (mathSymbol === "^"){
+			console.log(num1 ** num2)
+};
+
 
 	// This line closes the connection to the command line interface.
 	reader.close()
